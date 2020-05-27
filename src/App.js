b343d1a9e35d9ef37983a369c6bfae44ee28d20b
Taja House <ahlliahtaja@yahoo.com>
2020-05-26 17:33:42 -0700

    Set up logic from addItem function

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+ 
+
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
@@ -27,7 +29,8 @@ function App() {
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
-		</div>
+
+		</div>		
 	);
 }
 
